9f96eb9871d0de093d4ace695893231dbbbaa94c
wuyiming <1316399015@qq.com>
2023-08-18 17:19:05 +0800

    使用inputsystem与原输入方式对比

--- Assets/CreativeCore_Animation/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/CreativeCore_Animation/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
 
@@ -23,7 +23,7 @@ namespace StarterAssets
 		public bool cursorInputForLook = true;
 #endif
 
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+#if ENABLE_INPUT_SYSTEM
 		public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
